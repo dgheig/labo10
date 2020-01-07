ff9e2a5f919cfc996b4cab604d9b49856a432d16
Emmanuelle <Emmanuelle@ZBooky>
2020-01-07 15:25:09 +0100

    Fonctions display

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,10 +1,24 @@
 
 #include "geometry.h"
 
+using namespace std;
 
 void displayMap(const Map& map)
 {
+    for(size_t i = 0; i < map.size(); ++i)
+    {
+        displayLine(map[i]);
+        cout << endl;
+    }
 
+}
+
+void displayLine(const Line& line)
+{
+    for(size_t i = 0; i < line.size(); ++i)
+    {
+        cout << line[i] == EXIST ? 'O': '.';
+    }
 }
 
 void fillSpace(Space& space)
@@ -14,5 +28,5 @@ void fillSpace(Space& space)
 
 Map projection(const Space& space, MapCode code)
 {
-    return Map();
+    return space[code];
 }
