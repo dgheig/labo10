3666373eefaf7ae93fa48bd5b09b6d81f1ba90b5
Emmanuelle <Emmanuelle@ZBooky>
2020-01-09 09:02:01 +0100

    Put all {} in the same place

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -47,21 +47,18 @@ Coordinate getZ(Point point) {
     return 0;
 }
 
-int getNbrPoints()
-{
+int getNbrPoints() {
     int nbrPoints;
     bool valueOK;
     bool inputOK;
-    do
-    {
+    do {
         cout << "Please, enter the number of points you want : ";
-        cin >> nbrPoints;
+        cin  >> nbrPoints;
         cout << endl;
 
         inputOK = cin.good();
         valueOK = nbrPoints >= 0;
-        if(!inputOK)
-            cin.clear();
+        if(!inputOK) cin.clear();
         EMPTY_BUFFER;
 
     } while((!inputOK or !valueOK)); // and cout << "Incorrect input, please try again\n"
@@ -69,34 +66,29 @@ int getNbrPoints()
     return nbrPoints;
 }
 
-bool getPoint(int& x, int& y, int& z)
-{
+bool getPoint(int& x, int& y, int& z)   {
     cout << "Please, enter a point [X Y Z] : ";
     cin >> x >> y >> z;
     cout << endl;
     bool inputOK  = cin.good();
     bool valuesOK = x >= 0 or y >= 0 or z >= 0;
 
-    if(!inputOK)
-        cin.clear();
+    if(!inputOK) cin.clear();
     EMPTY_BUFFER;
 
     return (inputOK or valuesOK);
 }
 
-PointList getPointList()
-{
+PointList getPointList() {
     PointList list;
     int nbrPoints = getNbrPoints();
     list.resize(nbrPoints);
-    for(int i = 0; i < nbrPoints; ++i)
-    {
+    for(int i = 0; i < nbrPoints; ++i) {
         list[i].resize(3);
         int x, y, z;
         while(!getPoint(x, y, z))
-        {
             cout << "Incorrect input, please try again\n";
-        }
+
         list[i][0] = x;
         list[i][1] = y;
         list[i][2] = z;
@@ -105,26 +97,21 @@ PointList getPointList()
     return list;
 }
 
-void displayMap(const Map& map)
-{
-    for(size_t i = 0; i < map.size(); ++i)
-    {
+void displayMap(const Map& map) {
+    for(size_t i = 0; i < map.size(); ++i) {
         displayLine(map[i]);
         cout << endl;
     }
 
 }
 
-void displayLine(const Line& line)
-{
-    for(size_t i = 0; i < line.size(); ++i)
-    {
+void displayLine(const Line& line) {
+    for(size_t i = 0; i < line.size(); ++i) {
         cout << (line[i] == EXIST ? 'O': '.');
     }
 }
 
-void fillSpace(Space& space, PointList list)
-{
+void fillSpace(Space& space, PointList list) {
     for (auto point: list) {
         space[MAPCODE_XY][getX(point)][getY(point)] = EXIST;
         space[MAPCODE_YZ][getY(point)][getZ(point)] = EXIST;
