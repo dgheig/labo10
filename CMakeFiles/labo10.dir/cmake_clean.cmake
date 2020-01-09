e779a7776295b02f8d52a51cd0b7ab0003797a72
Gallay David <david.gallay@heig-vd.ch>
2020-01-09 16:53:49 +0100

    stable

---
+++ CMakeFiles/labo10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labo10.dir/labo_10_comte_emmanuelle_gallay_david.cpp.o"
+  "bin/labo10.pdb"
+  "bin/labo10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labo10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
